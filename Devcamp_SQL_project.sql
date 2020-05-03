a10207ecde6160aedd137865eec524fc06f378c7
DaironJimenez <55242140+DaironJimenez@users.noreply.github.com>
2020-05-03 12:42:49 -0400

    Add files via upload
    This is the SQL project

---
+++ Devcamp_SQL_project.sql
@@ -0,0 +1,25 @@
+Use SQl_project_devcamp
+
+INSERT INTO Grades (grades_students)
+VALUES ("Kristine 92")
+
+INSERT INTO Grades (grades_students)
+VALUES ("Dadas 94")
+
+INSERT INTO Grades (grades_students)
+VALUES ("Daniel 95")
+
+INSERT INTO Grades (grades_students)
+VALUES ("Michael 88")
+
+INSERT INTO Grades (grades_students)
+VALUES ("Eunice 98")
+
+
+INSERT INTO Grades (grades_students)
+VALUES ("Lucy 85")
+
+
+
+SELECT *
+FROM Grades;
